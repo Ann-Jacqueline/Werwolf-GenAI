c9cb087a8077eaa99d88d2006a32186977bc03ee
Seli Tusha <seli.tusha@gmail.com>
2024-12-27 12:46:53 +0100

    Moved db

--- backend/db/db_utils.py
+++ backend/db/db_utils.py
@@ -2,7 +2,8 @@ import sqlite3
 import os
 
 # Path to the database
-DB_PATH = os.path.join(os.path.dirname(__file__), 'werwolf.db')
+DB_PATH = os.path.join(os.path.dirname(__file__), '..', 'werwolf.db')
+DB_PATH = os.path.abspath(DB_PATH)  # Ensure it's an absolute path
 print(f"DEBUG: Database path is {DB_PATH}")
 
 
